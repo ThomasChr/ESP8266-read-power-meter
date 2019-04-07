8cf4b08c8dcc231b46e7ba76e44ff50b4a431eb1
Thomas Christlieb <ThomasChr@users.noreply.github.com>
2019-04-07 18:47:13 +0200

    prepare first value in powermail - untestet

--- powermail.php
+++ powermail.php
@@ -6,6 +6,9 @@ $dbuser = "envsensors";
 $dbpass = "4";
 $dbname = "envsensors";
 $fetchdays = 30;
+$cent_per_kwh = 27.88;
+$sensorid = 5;
+$mailrecp;
 
 /* connect db */
 $conn = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
@@ -14,6 +17,9 @@ if ($conn->connect_error) {
 }
 
 /* power yesterday */
+$yesterdayresult = $conn->query("select sum(kwh_since_last_send) from envsensors where sensorid = " . $sensorid . " and date(timestamp) = (curdate() - 1)");
+$yesterdayrow = $yesterdayresult->fetch_assoc();
+$yesterdaykwh = $yesterdayrow[0];
 
 /* power this week */
 
@@ -23,12 +29,14 @@ if ($conn->connect_error) {
 
 /* consumption for the last $fetchdays days */
 
-/* send mail */
+/* build the mailtext */
+$mailtext = "Verbrauch gestern: " . $yesterdaykwh . " kwh (" . ($yesterdaykwh * $cent_per_kwh / 100) . " EUR)\n";
 
+/* send mail */
+mail($mailrecp, "Stromdaten", $mailtext);
 
 echo "Done.";
 
-$stmt->close();
 $conn->close();
 
 ?>
