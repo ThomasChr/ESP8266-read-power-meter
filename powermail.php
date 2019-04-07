f9bb380651265f492d47e4e9c9faa92ffb15d697
Thomas Christlieb <ThomasChr@users.noreply.github.com>
2019-04-07 17:58:37 +0200

    prepare powermail (2)

--- powermail.php
+++ powermail.php
@@ -1,8 +1,34 @@
 <php
 
+/* globals */
+$dbserver = "localhost";
+$dbuser = "envsensors";
+$dbpass = "4";
+$dbname = "envsensors";
+$fetchdays = 30;
+
+/* connect db */
+$conn = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+if ($conn->connect_error) {
+    die("DB connect failed");
+}
+
+/* power yesterday */
+
+/* power this week */
+
+/* power this month */
+
+/* power this year */
+
+/* consumption for the last $fetchdays days */
+
+/* send mail */
 
 
+echo "Done.";
 
-
+$stmt->close();
+$conn->close();
 
 ?>
