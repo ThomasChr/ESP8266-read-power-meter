93e5046338e278d526eef9bce99380b6e4f03eb9
Thomas Christlieb <thomaschristlieb@hotmail.com>
2020-03-06 08:01:24 +0100

    Fix undefined errors

--- tempsensor.php
+++ tempsensor.php
@@ -6,44 +6,60 @@ $dbuser = "envsensors";
 $dbpass = "4";
 $dbname = "envsensors";
 
-$sensorid = $_POST["sensorid"];
-if (!isset($sensorid) || !is_numeric($sensorid)) {
-    die('Error 1');
+if ($_POST["sensorid"]) {
+    $sensorid = $_POST["sensorid"];
+    if (!isset($sensorid) || !is_numeric($sensorid)) {
+        die('Error 1');
+    }
 }
 
-$password = $_POST["password"];
-if (!isset($password) || !($password == $sensorpass)) {
-    die('Error 2');
+if ($_POST["password"]) {
+    $password = $_POST["password"];
+    if (!isset($password) || !($password == $sensorpass)) {
+        die('Error 2');
+    }
 }
 
-$temp = $_POST["temp"];
-if (!isset($temp) || !is_numeric($temp)) {
-    $temp = 0;
+if ($_POST["temp"]) {
+    $temp = $_POST["temp"];
+    if (!isset($temp) || !is_numeric($temp)) {
+        $temp = 0;
+    }
 }
 
-$press = $_POST["press"];
-if (!isset($press) || !is_numeric($press)) {
-    $press = 0;
+if ($_POST["press"]) {
+    $press = $_POST["press"];
+    if (!isset($press) || !is_numeric($press)) {
+        $press = 0;
+    }
 }
 
-$hum = $_POST["hum"];
-if (!isset($hum) || !is_numeric($hum)) {
-    $hum = 0;
+if ($_POST["hum"]) {
+    $hum = $_POST["hum"];
+    if (!isset($hum) || !is_numeric($hum)) {
+        $hum = 0;
+    }
 }
 
-$power = $_POST["power"];
-if (!isset($power) || !is_numeric($power)) {
-    $power = 0;
+if ($_POST["power"]) {
+    $power = $_POST["power"];
+    if (!isset($power) || !is_numeric($power)) {
+        $power = 0;
+    }
 }
 
-$kwh_since_start = $_POST["kwh_since_start"];
-if (!isset($kwh_since_start) || !is_numeric($kwh_since_start)) {
-    $kwh_since_start = 0;
+if ($_POST["kwh_since_start"]) {
+    $kwh_since_start = $_POST["kwh_since_start"];
+    if (!isset($kwh_since_start) || !is_numeric($kwh_since_start)) {
+        $kwh_since_start = 0;
+    }
 }
 
-$exceptiondata = $_POST["exceptiondata"];
-if (!isset($exceptiondata)) {
-    $exceptiondata = "";
+if ($_POST["exceptiondata"]) {
+    $exceptiondata = $_POST["exceptiondata"];
+    if (!isset($exceptiondata)) {
+        $exceptiondata = "";
+    }
 }
 
 $conn = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
