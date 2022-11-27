b7f970b46ce3b5d6b415bf0423a748a0cfd919c4
ThomasChr <thomaschristlieb@hotmail.com>
2022-11-27 11:41:44 +0100

    also print out how much has been paid already

--- powermail.php
+++ powermail.php
@@ -6,7 +6,7 @@ $dbuser = "envsensors";
 $dbpass = "4";
 $dbname = "envsensors";
 $fetchdays = 30;
-$fetchmonths = 12;
+$fetchmonths = 48;
 $CENT_PER_KWH = 38.54;
 $EURO_PER_MONTH = 10.50;
 $EURO_PER_MONTH_PREPAYMENT = 85;
@@ -58,7 +58,7 @@ while($daysrow = $daysresult->fetch_assoc()) {
 $monthconsumption = "";
 $monthsresult = $conn->query("select sum(kwh_since_last_send) as sum, DATE_FORMAT(timestamp,'%c/%y') as month from sensorvalues where sensorid = " . $sensorid . " group by 2 order by timestamp DESC LIMIT 0, " . $fetchmonths . ";");
 while($monthsrow = $monthsresult->fetch_assoc()) {
-    $monthconsumption .= $monthsrow["month"] . ": " . round($monthsrow["sum"], 2) . " kWh (" . round(($monthsrow["sum"] * $CENT_PER_KWH / 100 + $EURO_PER_MONTH), 2) . " EUR)\n";
+    $monthconsumption .= $monthsrow["month"] . ": " . round($monthsrow["sum"], 2) . " kWh (" . round(($monthsrow["sum"] * $CENT_PER_KWH / 100 + $EURO_PER_MONTH), 2) . " EUR - " . round($EURO_PER_MONTH_PREPAYMENT, 2) . " EUR = " . round(($monthsrow["sum"] * $CENT_PER_KWH / 100 + $EURO_PER_MONTH) - ($EURO_PER_MONTH_PREPAYMENT), 2) . " EUR)\n";
 }
 
 /* build the mailtext */
@@ -66,9 +66,9 @@ $mailtext = "Es ist der " . date("d.m.Y", $starttime) . ":\n";
 $mailtext .= "Verbrauch gestern: " . round($yesterdaykwh, 2) . " kWh (" . round(($yesterdaykwh * $CENT_PER_KWH / 100), 2) . " EUR)\n";
 $mailtext .= "Verbrauch diese Woche: " . round($thisweekkwh, 2) . " kWh (" . round(($thisweekkwh * $CENT_PER_KWH / 100), 2) . " EUR)\n";
 $mailtext .= "Verbrauch dieses Monat: " . round($thismonthkwh, 2) . " kWh (" . round(($thismonthkwh * $CENT_PER_KWH / 100 + $EURO_PER_MONTH), 2) . " EUR)\n";
-$mailtext .= "Verbrauch dieses Jahr: " . round($thisyearkwh, 2) . " kWh (" . round(($thisyearkwh * $CENT_PER_KWH / 100 + ($EURO_PER_MONTH * date('m'))), 2) . " EUR)\n\n";
+$mailtext .= "Verbrauch dieses Jahr: " . round($thisyearkwh, 2) . " kWh (" . round(($thisyearkwh * $CENT_PER_KWH / 100 + ($EURO_PER_MONTH * date('m'))), 2) . " EUR - " . round($EURO_PER_MONTH_PREPAYMENT * date('m'), 2) . " EUR = " . round(($thisyearkwh * $CENT_PER_KWH / 100 + ($EURO_PER_MONTH * date('m'))) - ($EURO_PER_MONTH_PREPAYMENT * date('m')), 2) . " EUR)\n\n";
 $mailtext .= "Preis pro kWh: " . $CENT_PER_KWH . " Cent\n";
-$mailtext .= "Grudngebuehr pro Monat: " . $EURO_PER_MONTH . " Euro\n";
+$mailtext .= "Grundgebuehr pro Monat: " . $EURO_PER_MONTH . " Euro\n";
 $mailtext .= "Abschlag pro Monat: " . $EURO_PER_MONTH_PREPAYMENT . " Euro\n\n";
 $mailtext .= "Verbrauch gestern:\n";
 $mailtext .= $yesterdayhourconsumption . "\n\n";
